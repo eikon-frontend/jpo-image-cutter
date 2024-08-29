39ba5c6bffec05eb4599f7ec6ced4ae90ec54274
Nicolas Buntschu <nicolas.buntschu@gmail.com>
2024-08-29 11:52:52 +0200

    Extract square from image

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 import fs from "fs";
+import sharp from "sharp";
+
+// Formats acceptés
+const formats = ["jpg", "jpeg", "png", "gif"];
+
+// Fonction d'extraction d'une partie de l'image
+const extract = async (file) => {
+  const input = await sharp(`images/${file}`).toBuffer();
+
+  await sharp(input)
+    .extract({
+      left: 0,
+      top: 0,
+      width: 50,
+      height: 50,
+    })
+    .toFile(`extracts/${file}`);
+};
 
 fs.readdir("images", (err, files) => {
   if (err) {
@@ -6,7 +24,13 @@ fs.readdir("images", (err, files) => {
     return;
   }
 
+  // On filtre la liste des fichiers pour ne garder que les formats acceptés
+  files = files.filter((file) => {
+    const extension = file.split(".").pop();
+    return formats.includes(extension);
+  });
+
   files.forEach((file) => {
-    console.log(file);
+    extract(file);
   });
 });
