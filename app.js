630d64cb1d1e66413ca29e548d49893cd34597a6
Nicolas Buntschu <nicolas.buntschu@gmail.com>
2024-08-29 11:38:01 +0200

    Get files from images folder

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import fs from "fs";
+
+fs.readdir("images", (err, files) => {
+  if (err) {
+    console.error("Impossible de lire le dossier images");
+    return;
+  }
+
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
